9732dab75440f56d805077c4892430a162852192
danyu-pds <51976247+danyu-pds@users.noreply.github.com>
2022-06-14 14:06:02 -0700

    Update imgspec_run.sh
    Updated the script compatible with all the instruments and to rename the output products with just a simple “_grainsize” extension (removing any previous extensions if they exist).

--- .imgspec/imgspec_run.sh
+++ .imgspec/imgspec_run.sh
@@ -3,16 +3,29 @@
 sister_dir=$( cd "$(dirname "$0")" ; pwd -P )
 grainsize_pge_dir=$(dirname ${sister_dir})
 
-out_dir='output'
-rfl_file=input/PRS_20210302180507_20210302180511_0001_rfl_prj_rfl
+for a in `ls -1 input/*.tar.gz`; do tar -xzvf $a -C input; done
 
-mkdir -p $out_dir
+rfl_file=$(ls input/*/*rfl)
+file_base=$(basename $rfl_file)
+
+if [[ $file_base == f* ]]; then
+    output_prefix=$(echo $file_base | cut -c1-16)
+elif [[ $file_base == ang* ]]; then
+    output_prefix=$(echo $file_base | cut -c1-18)
+elif [[ $file_base == PRS* ]]; then
+    output_prefix=$(echo $file_base | cut -c1-38)
+elif [[ $file_base == DESIS* ]]; then
+    output_prefix=$(echo $file_base | cut -c1-44)
+fi
+
+out_dir=${output_prefix}_grainsize
+mkdir -p output/${out_dir}
 
 # Run Grain Size PGE, export L2A_SI_TRAIT file
-python ${grainsize_pge_dir}/grainsize.py $rfl_file $out_dir/ --verbose
+python ${grainsize_pge_dir}/grainsize.py $rfl_file output/${out_dir} --verbose
 
 # gzip output files in preparation for downstream processing
-#cd $out_dir
-tar -cf ${out_dir}.tar ${out_dir}
+cd out_dir
+tar -czvf ${out_dir}.tar.gz ${out_dir}
 rm -rf $out_dir
-gzip ${out_dir}.tar
+# gzip ${out_dir}.tar.gz
